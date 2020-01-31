8d79ed0162bd3248a0ab4b9e8c6d05fe5b1136ee
gelbman20 <gelbman20@gmail.com>
2020-01-31 14:12:18 +0200

    compile pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	context: path.resolve(__dirname, 'src'),
+	mode: 'development',
+	entry: './index.js',
 	output: {
-		filename: "bundle.js",
+		filename: "[name].[hash].bundle.js",
 		path: path.resolve(__dirname, 'docs')
-	}
+	},
+	module: {
+		rules: [
+			{ test: /\.(pug|jade)$/, use: ['pug-loader'] },
+			{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
+			{ test: /\.(png|jpe?g|gif)$/i, use: 'file-loader' }
+		]
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './assets/pug/pages/index.pug',
+			filename: 'index.html'
+		}),
+		new HTMLWebpackPlugin({
+			template: './assets/pug/pages/in-game.pug',
+			filename: 'in-game.html'
+		}),
+		new HTMLWebpackPlugin({
+			template: './assets/pug/pages/match-ended.pug',
+			filename: 'match-ended.html'
+		}),
+		new CleanWebpackPlugin()
+	]
 };
